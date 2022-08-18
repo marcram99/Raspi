64d14e89c318f0dfcf7e788fe6903d550443301f
Marc Wanner <marcram99@gmail.com>
2022-08-18 17:28:15 +0200

    start working on light_logger

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 #configuration des différents chemins d'accès des logfiles
 class Config():
-    logfiles_path = Path.home().joinpath('Raspi/logfiles')
+    logfiles_path = Path.home().joinpath('Marc-perso/code/python/Raspi/logfiles')
     if not logfiles_path.exists():
         Path.mkdir(logfiles_path)
 
     light_logfile = logfiles_path.joinpath(f'{datetime.now():%Y-%m}_capt02.log')
     temp_logfile = logfiles_path.joinpath(f'{datetime.now():%Y-%m-%d}_capt01.log')    
     debug_logfile = logfiles_path.joinpath('00_DEBUG_logfile.log')
-    data_file = Path.home().joinpath('Raspi/data.json')
+    data_file = Path.home().joinpath('Marc-perso/code/python/Raspi/data.json')
 
--- light_loggeer.py
+++ light_loggeer.py
@@ -1,6 +1,19 @@
+import json
 from datetime import datetime
 from pathlib import Path
 
 from config import Config
 
 print('LIGHT LOGGER')
+log_file = Config.light_logfile
+data_file = Config.data_file
+if not data_file.exists():
+    with open(data_file,'w') as new_file:
+        json.dumps({'light_mode':'dark',
+                    'change_time':'',
+                    'mail_time': '',
+                    'mail_alert':0
+                    },new_file)
+print(data_file)
+with open(data_file) as json_file:
+    data = json.load(json_file)
