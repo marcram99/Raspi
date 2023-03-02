adacbd9635956d5348af2b8776e399c02a642387
marcram99 <marcram99@gmail.com>
2023-03-02 22:23:23 +0100

    compteur nb logfile

--- log_sender.py
+++ log_sender.py
@@ -3,3 +3,5 @@ from pathlib import Path
 log_path = Path.home().joinpath('Raspi/logfiles')
 content = list(log_path.glob('*.log'))
 print(content)
+print(20*'-')
+print(f'nb de fichier:{len(content)}')
