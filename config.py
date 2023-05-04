b40fc101a61173029dc11ac4f7723bfb74aff44f
marcram99 <marcram@proton.me>
2023-05-04 12:55:40 +0200

    log_sender: begin ssh com. with key

--- config.py
+++ config.py
@@ -5,12 +5,16 @@ from pathlib import Path
 
 
 class Config():
+    # macbook path:
+    files_path = Path.home().joinpath('Documents/code/python/Raspi/files')
+    logfiles_path = Path.home().joinpath('Documents/code/python/Raspi/logfiles')
+    ssh_key = Path.home().joinpath('.ssh/id_ed25519')
     # mac path:
     #files_path = Path.home().joinpath('Marc-perso/code/python/Raspi/files')
     #logfiles_path = Path.home().joinpath('Marc-perso/code/python/Raspi/logfiles')
     # linux path:
-    files_path = Path.home().joinpath('Raspi/files')
-    logfiles_path = Path.home().joinpath('Raspi/logfiles')
+    #files_path = Path.home().joinpath('Raspi/files')
+    #logfiles_path = Path.home().joinpath('Raspi/logfiles')
     if not files_path.exists():
         Path.mkdir(files_path)
     if not logfiles_path.exists():
--- log_sender.py
+++ log_sender.py
@@ -1,7 +1,28 @@
 from pathlib import Path
+from paramiko import SSHClient, ed25519key, AutoAddPolicy
+from config import Config
+
+ssh_key = Config.ssh_key
+private_key = ed25519key.Ed25519Key.from_private_key_file(ssh_key)
+client = SSHClient()
+client.set_missing_host_key_policy(AutoAddPolicy)
+cmd = 'ls -l'
+print('---------- start SSH connexion----------')
+client.connect('46.226.104.139', username='ubuntu', pkey=private_key)
+sftp_client = client.open_sftp()
+# sftp_client.get(remote_path, output_file)
+# sftp_client.put(local_path, remote_path)
+client.close()
+print('---------- SSH connexion closed ----------')
+
+"""
+stdin, stdout, stderr = client.exec_command(cmd)
+output = stdout.read()
+print(str(output, 'utf8'))
 
 log_path = Path.home().joinpath('Raspi/logfiles')
 content = list(log_path.glob('*.log'))
 print(content)
 print(20*'-')
 print(f'nb de fichier:{len(content)}')
+"""
